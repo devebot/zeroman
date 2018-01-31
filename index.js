d7d099fe07244c67b342de5b1fda52f73f5285cb
pnhung177 <pnhung177@acegik.net>
2018-01-31 23:54:40 +0700

    provide zeroman constructor for inheritance

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-module.exports = require('./lib/_index');
+'use strict';
+
+var Constructor = require('./constructor');
+
+var _instance = new Constructor();
+
+Object.defineProperty(_instance, 'new', {
+  get: function() {
+    return function(kwargs) {
+      return new Constructor(kwargs);
+    }
+  },
+  set: function(val) {}
+});
+
+module.exports = _instance;
